605dba2a6077a5db8f88f8729efcde2d5b65795c
Hamdan Nawaz <hamdannawaz582@gmail.com>
2026-04-17 22:28:41 +0500

    forgot the to_code method

--- components/haierac160/climate.py
+++ components/haierac160/climate.py
@@ -6,3 +6,6 @@ AUTO_LOAD = ["climate_ir"]
 haierac160 = cg.esphome_ns.namespace("haierac160")
 HaierAC160Climate = haierac160.class_("HaierAC160Climate", climate_ir.ClimateIR)
 CONFIG_SCHEMA = climate_ir.climate_ir_schema(HaierAC160Climate)
+
+async def to_code(config):
+    await climate_ir.new_climate_ir(config)
